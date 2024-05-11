521e37a0a959a36c5781339249a83b3930a0c1fe
PabbliSS <pablosanchezsequera@gmail.com>
2024-05-11 15:02:20 +0200

    ProyectoB.java

--- Empresa B/ProyectoB.java
+++ Empresa B/ProyectoB.java
@@ -3,6 +3,9 @@ public class ProyectoB {
     public static void main(String[] args) {
         
         System.out.println("Este es el Proyecto B.");
+        System.out.println("Este es una nueva edicion desde GitHub");
+
+        //Tags
 
     }
 
